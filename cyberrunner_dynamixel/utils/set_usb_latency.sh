ad5a53d3d940d4cb90c0ab716da72e49de8c8d7b
BKaropczyc <60446046+BKaropczyc@users.noreply.github.com>
2025-07-02 14:46:45 -0400

    Fixed the wording of the messages.

--- cyberrunner_dynamixel/utils/set_usb_latency.sh
+++ cyberrunner_dynamixel/utils/set_usb_latency.sh
@@ -3,8 +3,8 @@ dev=ttyUSB0
 if [ $# -ge 1 ];then
   dev=$1
 fi
-echo "Checking latency of $dev..."
+echo "Checking the latency of $dev..."
 if [ -f /sys/bus/usb-serial/devices/$dev/latency_timer ] && [ `cat /sys/bus/usb-serial/devices/$dev/latency_timer` -gt 1 ];then
-  echo "Setting the latency issue of $dev..."
+  echo "Setting the latency of $dev..."
   echo 1 | sudo tee /sys/bus/usb-serial/devices/$dev/latency_timer
 fi
