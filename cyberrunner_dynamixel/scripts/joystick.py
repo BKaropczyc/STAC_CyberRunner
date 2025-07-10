2e407d6e5375dd54ce54e86b7d1748b4285bbec8
BKaropczyc <bkaropcz@stac.edu>
2025-07-10 19:38:07 -0400

    Improved joystick behavior: - Increased the maximum velocity to send - We now find the first joystick device with "joystick" in its name, to avoid selecting invalid joystick devices - We now receive joystick events even when the game window is not activated - Added filtering of redundant messages due to joystick jitter / noise

--- cyberrunner_dynamixel/scripts/joystick.py
+++ cyberrunner_dynamixel/scripts/joystick.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import os
 import pygame
 import rclpy
 from rclpy.node import Node
@@ -16,23 +17,32 @@ def main(args=None):
     pub = node.create_publisher(DynamixelVel, "cyberrunner_dynamixel/cmd", 10)
     msg = DynamixelVel()
 
-    # Define a maximum servo velocity to send
-    max_vel = 50.0
+    # Parameters that impact the publishing of our ROS messages
+    max_vel = 100.0     # Maximum servo velocity to send
+    min_diff = 0.005    # Minimum difference in the JOYAXISMOTION values that will cause us to publish a new message
+                        # Increasing this value prevents publishing many messages due to joystick jitter / noise
+
+    # Allow pygame to receive joystick events even when the game window is not activated
+    os.environ["SDL_JOYSTICK_ALLOW_BACKGROUND_EVENTS"] = "1"
 
     # Initialize the pygame module
     pygame.init()
-    pygame.joystick.init()
     clock = pygame.time.Clock()
 
     # Find an available joystick
     joystick_count = pygame.joystick.get_count()
-    if joystick_count == 0:
-        print("No joysticks found.")
+    for i in range(joystick_count):
+        joystick = pygame.joystick.Joystick(i)
+        joystick_name = joystick.get_name()
+        if "joystick" in joystick_name.lower():
+            # This looks like a joystick we can use
+            joystick.init()
+            print(f"Opened joystick: {joystick_name}")
+            break
     else:
-        # Assuming you want the first detected joystick
-        joystick = pygame.joystick.Joystick(0)
-        joystick.init()
-        print(f"Opened joystick: {joystick.get_name()}")
+        # No valid joysticks were found
+        print("No acceptable joysticks found.")
+        exit(1)
 
     # Create game screen
     window_size = (640, 100)
@@ -48,13 +58,17 @@ def main(args=None):
     pygame.display.update()
 
     # Keep track of the latest position of each joystick axis
-    latest_v_pos = 0.0
     latest_h_pos = 0.0
+    latest_v_pos = 0.0
 
     # Main loop:
     running = True
     while running:
+        # Limit the game to 60 fps
         clock.tick(60)
+
+        # Only publish a new message if we received an applicable event
+        pub_msg = False
 
         for event in pygame.event.get():
             # If the user closes the pygame window, stop the script
@@ -67,15 +81,20 @@ def main(args=None):
                 value = event.value
 
                 # Update our joystick positions
-                if axis == 1:
-                    latest_v_pos = value
-                elif axis == 3:
-                    latest_h_pos = value
+                if axis == 0:
+                    if abs(value - latest_h_pos) > min_diff:
+                        latest_h_pos = value
+                        pub_msg = True
+                elif axis == 1:
+                    if abs(value - latest_v_pos) > min_diff:
+                        latest_v_pos = value
+                        pub_msg = True
 
-        # Publish a velocity message to move the servos
-        msg.vel_1 = latest_h_pos * max_vel
-        msg.vel_2 = latest_v_pos * max_vel
-        pub.publish(msg)
+        if pub_msg:
+            # Publish a velocity message to move the servos
+            msg.vel_1 = latest_h_pos * max_vel
+            msg.vel_2 = latest_v_pos * max_vel
+            pub.publish(msg)
 
     # Stop the servos
     msg.vel_1 = 0.0
